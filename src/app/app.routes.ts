6262c7a440bf25e500c1bbdd32aa7fb0d2ca6e0a
IvoCorrea <ivoaurelio30@gmail.com>
2026-04-25 02:35:28 -0300

    feat(auth): implement RegisterComponent for account creation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,11 +7,11 @@ export const routes: Routes = [
         redirectTo: '/dashboard',
         pathMatch: 'full',
     },
-    /*
+    
     {
       path: 'login',
       loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent),
-    },
+    },/*
     {
       path: 'register',
       loadComponent: () => import('./pages/register/register.component').then(m => m.RegisterComponent),
